dbb70f5964f4880c319f0ad87849f15d4d2f6d06
Shane <6071159+smashedr@users.noreply.github.com>
2023-12-11 14:03:42 -0800

    Pushing Current Changes

--- simpleflying/simpleflying.js
+++ simpleflying/simpleflying.js
@@ -3,7 +3,7 @@
 // @description    Automatically delete unauthenicatedArticleLimitReached cookie.
 // @version        20231101
 // @author         shane
-// @icon           https://imdb.com/favicon.ico
+// @icon           https://simpleflying.com/public/build/images/favicon-48x48.8d7facc6.png
 // @namespace      https://github.com/cssnr/browser-scripts/simpleflying-extras.js
 // @match          *://*.simpleflying.com/*
 // @noframes
@@ -11,81 +11,29 @@
 
 'use strict'
 
-// Wait for load
-window.addEventListener(
-    'load',
-    function () {
-        //--- Loop through cookies and delete them.
-        const cookieList = document.cookie.split(/;\s*/)
-        console.log('cookieList:', cookieList)
-
-        for (let J = cookieList.length - 1; J >= 0; --J) {
-            const cookieName = cookieList[J].replace(/\s*(\w+)=.+$/, '$1')
-            console.log('cookieName:', cookieName)
-            // eraseCookie(cookieName)
-            if (cookieName === 'unauthenicatedArticleLimitReached') {
-                console.log(`Deleting Cookie: ${cookieName}`)
+window.addEventListener('load', () => {
+    // console.log('document.cookie:', document.cookie)
+    let cookies = document.cookie.split(';').map((item) => item.trim())
+    console.log('cookies:', cookies)
+    for (const cookie of cookies) {
+        // console.log('cookie:', cookie)
+        let [name, value] = cookie.split(/=(.*)/s)
+        // console.log(`${name}: ${value}`)
+        if (name === 'unauthenicatedArticleLimitReached') {
+            if (value === 'true') {
+                document.cookie = 'unauthenicatedArticleLimitReached=false'
+                console.log('Reset Article Limit')
             }
+        } else if (name === 'articlesRead') {
+            const decodedString = decodeURIComponent(value)
+            const decodedObject = JSON.parse(decodedString)
+            console.log('articlesRead:', decodedObject)
+            // decodedObject.visited_links = []
+            const jsonString = JSON.stringify(decodedObject)
+            const encodedString = encodeURIComponent(jsonString)
+            console.log('encodedString:', encodedString)
+            document.cookie = `articlesRead=${encodedString}`
+            console.log('Reset Visited Links')
         }
-
-        console.log('cookieList:', cookieList)
-    },
-    false
-)
-
-// function eraseCookie(cookieName) {
-//     //--- ONE-TIME INITS:
-//     //--- Set possible domains. Omits some rare edge cases.?.
-//     var domain = document.domain
-//     var domain2 = document.domain.replace(/^www\./, '')
-//     var domain3 = document.domain.replace(/^(\w+\.)+?(\w+\.\w+)$/, '$2')
-//
-//     //--- Get possible paths for the current page:
-//     var pathNodes = location.pathname.split('/').map(function (pathWord) {
-//         return '/' + pathWord
-//     })
-//     var cookPaths = [''].concat(
-//         pathNodes.map(function (pathNode) {
-//             if (this.pathStr) {
-//                 this.pathStr += pathNode
-//             } else {
-//                 this.pathStr = '; path='
-//                 return this.pathStr + pathNode
-//             }
-//             return this.pathStr
-//         })
-//     )
-//
-//     ;(eraseCookie = function (cookieName) {
-//         //--- For each path, attempt to delete the cookie.
-//         cookPaths.forEach(function (pathStr) {
-//             //--- To delete a cookie, set its expiration date to a past value.
-//             document.cookie =
-//                 cookieName +
-//                 '=' +
-//                 pathStr +
-//                 '; expires=Thu, 01-Jan-1970 00:00:01 GMT;'
-//             document.cookie =
-//                 cookieName +
-//                 '=' +
-//                 pathStr +
-//                 '; domain=' +
-//                 domain +
-//                 '; expires=Thu, 01-Jan-1970 00:00:01 GMT;'
-//             document.cookie =
-//                 cookieName +
-//                 '=' +
-//                 pathStr +
-//                 '; domain=' +
-//                 domain2 +
-//                 '; expires=Thu, 01-Jan-1970 00:00:01 GMT;'
-//             document.cookie =
-//                 cookieName +
-//                 '=' +
-//                 pathStr +
-//                 '; domain=' +
-//                 domain3 +
-//                 '; expires=Thu, 01-Jan-1970 00:00:01 GMT;'
-//         })
-//     })(cookieName)
-// }
+    }
+})
