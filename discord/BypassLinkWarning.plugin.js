0635ba7420b873176213702c3d29174238602948
Shane <6071159+smashedr@users.noreply.github.com>
2024-05-22 22:40:17 -0700

    Add Better Discord Plugin

---
+++ discord/BypassLinkWarning.plugin.js
@@ -0,0 +1,52 @@
+/**
+ * @name BypassLinkWarning
+ * @version 1.0.1
+ * @description Automatically Click Visit Site on the Leaving Discord screen.
+ * @author Shane
+ */
+
+module.exports = class BypassLinkWarning {
+    constructor() {}
+
+    load() {}
+
+    start() {
+        this.observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.addedNodes.length) {
+                    mutation.addedNodes.forEach((node) => {
+                        if (node.nodeType === 1) {
+                            processNode(node)
+                        }
+                    })
+                }
+            })
+        })
+
+        this.observe()
+    }
+
+    stop() {
+        this.observer.disconnect()
+    }
+
+    observe() {
+        const targetNode = document.body
+        const config = { childList: true, subtree: true }
+
+        this.observer.observe(targetNode, config)
+    }
+}
+
+function processNode(node) {
+    const button = node.querySelector('button[type="button"]')
+    if (button?.textContent?.includes('Visit Site')) {
+        button.click()
+        const bg = document.querySelector('[class^="backdrop"]')
+        if (bg) {
+            setTimeout(() => {
+                bg.click()
+            }, 50)
+        }
+    }
+}
